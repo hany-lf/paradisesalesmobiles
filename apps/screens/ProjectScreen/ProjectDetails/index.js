bd10ce397da9d85e4c93dc5f21e35b9a93970e01
haniyya ulfah <haniyyaulfah@ymail.com>
2023-09-20 16:47:57 +0700

    20/9, modal project detail overview done

--- apps/screens/ProjectScreen/ProjectDetails/index.js
+++ apps/screens/ProjectScreen/ProjectDetails/index.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Alert,
   StyleSheet,
+  Modal,
 } from 'react-native';
 import styles from './styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -18,41 +19,9 @@ import {ButtonMenuHome} from '../../../components';
 import YoutubePlayer from 'react-native-youtube-iframe';
 import React, {useState, useCallback, useRef} from 'react';
 
-import {
-  iframeModel,
-  HTMLIframe,
-  useHtmlIframeProps,
-} from '@native-html/iframe-plugin';
-import RenderHTML, {
-  RenderHTMLProps,
-  CustomRendererProps,
-  TBlock,
-  CustomBlockRenderer,
-} from 'react-native-render-html';
-import {WebView} from 'react-native-webview';
-
-const FixedIFrameRenderer: CustomBlockRenderer = props => {
-  const iframeProps = useHtmlIframeProps(props);
-  if (!iframeProps) {
-    return null;
-  }
-  const {width, height} = StyleSheet.flatten(iframeProps.style);
-  return (
-    <View style={{width, height}}>
-      <HTMLIframe {...iframeProps} />
-    </View>
-  );
-};
-
-const htmlConfig: Partial<RenderHTMLProps> = {
-  renderers: {
-    iframe: FixedIFrameRenderer,
-  },
-  customHTMLElementModels: {
-    iframe: iframeModel,
-  },
-  WebView,
-};
+import IframeRenderer, {iframeModel} from '@native-html/iframe-plugin';
+import RenderHTML from 'react-native-render-html';
+import WebView from 'react-native-webview';
 
 const ProjectDetails = props => {
   console.log('props dari project', props);
@@ -61,18 +30,9 @@ const ProjectDetails = props => {
   const [playing, setPlaying] = useState(false);
   const {width} = Dimensions.get('screen').width;
   const paramsDetail = props.route.params;
+  const [modalVisible, setModalVisible] = useState(false);
 
   const source_video = 'https://www.youtube.com/watch?v=R8JLo2EB3Wk&t=8s';
-
-  const htmlContent = `
-   
-    <p>iframe</p>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.454417604291!2d106.8161527!3d-6.2156871!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f154ce1b1f43%3A0xf301b1f204447127!2sSudirman%20Suites%20Apartment!5e0!3m2!1sid!2sid!4v1695113732314!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-   
-    <p>end iframe</p>
-   
-    
-`;
 
   const onStateChange = useCallback(state => {
     if (state === 'ended') {
@@ -84,6 +44,14 @@ const ProjectDetails = props => {
   const togglePlaying = useCallback(() => {
     setPlaying(prev => !prev);
   }, []);
+
+  const renderers = {
+    iframe: IframeRenderer,
+  };
+
+  const customHTMLElementModels = {
+    iframe: iframeModel,
+  };
 
   return (
     <SafeAreaView
@@ -193,7 +161,7 @@ const ProjectDetails = props => {
             vulputate nunc. Quisque ac sed lectus eu adipiscing tellus lacus
             diam.
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
             <View
               style={{
                 flexDirection: 'row',
@@ -261,14 +229,13 @@ const ProjectDetails = props => {
               }></ButtonMenuHome>
           </View>
         </View>
-
         {/* /// VIDEO  */}
         <View
           style={{
             marginHorizontal: 20,
-            borderRadius: 15,
+            // borderRadius: 15,
             marginTop: 15,
-            backgroundColor: 'yellow',
+            // backgroundColor: 'yellow',
             marginBottom: 0,
           }}>
           <Text
@@ -294,31 +261,154 @@ const ProjectDetails = props => {
           </Button> */}
           {/* <Button title={playing ? 'pause' : 'play'} onPress={togglePlaying} /> */}
         </View>
-
         {/* /// LOCATION  */}
         <View>
-          <Text>mau lokasi</Text>
-          {/* <WebView
-            originWhitelist={['*']}
-            source={{html: '<h1>Hello    world</h1>'}}
-          /> */}
-          {/* <RenderHTML
-            // customHTMLElementModels={HTMLElementModel}
-            contentWidth={width}
-            source={{
-              html: '../tes.html',
-            }}></RenderHTML> */}
-          {/* <RenderHTML
-            source={{
-              html: '<iframe width="100%" height="50%" src="https://www.youtube.com/embed/cqyziA30whE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-            }}
-            style={{marginTop: 20}}
-          /> */}
-          {/* <RenderHTML
-            {...htmlConfig}
-            source={{html: htmlContent}}
-            contentWidth={Dimensions.get('window').width}
-          /> */}
+          <View
+            style={{
+              marginHorizontal: 20,
+              // borderRadius: 15,
+              marginTop: 15,
+              // backgroundColor: 'yellow',
+              marginBottom: 0,
+            }}>
+            <Text
+              style={{
+                fontFamily: Fonts.type.LatoBold,
+                fontSize: 14,
+                color: BaseColor.corn70,
+                marginVertical: 15,
+              }}>
+              Location
+            </Text>
+
+            <RenderHTML
+              renderers={renderers}
+              WebView={WebView}
+              contentWidth={width}
+              customHTMLElementModels={customHTMLElementModels}
+              defaultWebViewProps={
+                {
+                  /* Any prop you want to pass to all WebViews */
+                }
+              }
+              renderersProps={{
+                iframe: {
+                  scalesPageToFit: true,
+                  webViewProps: {
+                    /* Any prop you want to pass to iframe WebViews */
+                  },
+                },
+              }}
+              source={{
+                html: `<!DOCTYPE html>
+                            <html>
+                            <body>
+                            
+                            <h1>The iframe element</h1>
+                            
+                            <iframe width="100%" height="315"
+                                src="https://stackoverflow.com/questions/64057689/iframe-not-showing-in-react-native-webview">
+                            </iframe>
+                            
+                            </body>
+                            </html>`,
+                // ' <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>',
+              }}></RenderHTML>
+          </View>
+        </View>
+        {/* // --- modal project detail overview */}
+        <View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}>
+            <View
+              style={[
+                styles.centeredView,
+                {
+                  backgroundColor: BaseColor.whiteColor,
+                  borderTopRightRadius: 25,
+                  borderTopLeftRadius: 25,
+                },
+              ]}>
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginHorizontal: 20,
+                    marginVertical: 20,
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <View>
+                      <Icon
+                        name={'arrow-left'}
+                        size={18}
+                        color={BaseColor.corn90}></Icon>
+                    </View>
+                  </TouchableOpacity>
+
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text
+                      style={{
+                        fontFamily: Fonts.type.LatoBold,
+                        color: BaseColor.corn70,
+                        fontSize: 16,
+                      }}>
+                      Overview
+                    </Text>
+                  </View>
+                </View>
+                {/* --- border  */}
+                <View
+                  style={{
+                    borderWidth: 0.3,
+                    borderColor: BaseColor.corn70,
+                    borderStyle: 'solid',
+                  }}></View>
+              </View>
+              {/* <View style={styles.modalView}> */}
+              <View style={{marginHorizontal: 30, marginVertical: 20}}>
+                <Text
+                  style={{textAlign: 'justify', fontFamily: Fonts.type.Lato}}>
+                  Lorem ipsum dolor sit amet consectetur. Nibh scelerisque
+                  tristique facilisi lectus ullamcorper. Commodo sed egestas ut
+                  ullamcorper vulputate enim dui. Nec tristique venenatis
+                  euismod ut volutpat sapien consectetur eu. Ornare pharetra
+                  netus pellentesque sit at aliquam scelerisque. Non aliquet
+                  libero bibendum sagittis est sapien tempor. Viverra nullam
+                  mollis nulla lacus scelerisque. Est magna massa libero orci
+                  egestas. Nibh in et egestas odio platea sit. Nulla adipiscing
+                  aliquet ac hac aliquam nunc neque. Sed euismod leo adipiscing
+                  donec metus pretium fermentum in. Euismod turpis ullamcorper
+                  egestas lorem. Ut tellus posuere lacus pharetra orci et ac.
+                  Curabitur massa volutpat ac volutpat porttitor. Amet venenatis
+                  neque tempus dui ultrices viverra. Orci sit adipiscing congue
+                  lectus eu consectetur ornare. Dignissim ullamcorper lacinia
+                  eget porttitor volutpat dui faucibus. Dictum quam convallis in
+                  suspendisse diam volutpat diam. Rutrum tempus suspendisse nunc
+                  aliquam scelerisque mauris integer sit arcu. Egestas sagittis
+                  velit nunc dolor praesent. Diam sem maecenas eleifend ut.
+                  Tellus pretium vestibulum nisi ac urna neque viverra ac.
+                  Viverra fames scelerisque laoreet nisi ut viverra. Nunc nunc
+                  ipsum nisl mi facilisis mattis ac. Mi sed est ut non lobortis.
+                  Dignissim elit molestie vulputate pellentesque phasellus diam
+                  turpis leo.
+                </Text>
+                {/* <Text style={styles.modalText}>Hello World!</Text>
+                <Button
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Button> */}
+              </View>
+            </View>
+          </Modal>
         </View>
       </ScrollView>
     </SafeAreaView>
--- apps/screens/ProjectScreen/ProjectDetails/styles.js
+++ apps/screens/ProjectScreen/ProjectDetails/styles.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet, Dimensions, Platform} from 'react-native';
 import {BaseColor, Fonts} from '@config';
 const {width: screenWidth} = Dimensions.get('window');
 export default StyleSheet.create({
@@ -67,5 +67,46 @@ export default StyleSheet.create({
     resizeMode: 'contain',
     // width: screenWidth - 50,
     // height: screenWidth - 70,
+  },
+  centeredView: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
